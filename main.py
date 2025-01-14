5ab689d2d48656f95ba4a824bd2b8d26a7d72d35
Jimmy <jmart792@calpoly.edu>
2025-01-14 10:33:02 -0800

    Changed Email to school one

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Input: a name as a string
 # Result: a string
 def welcome_message(name:str) -> str:
-   message = "Hello, " + name + "Jimmy."
+   message = "Hello, " + name + "Jimmy.."
    return message
 
 
